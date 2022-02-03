bb71f16b9a136fe421f0fbda5e3244995c6f164d
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-02-03 14:50:56 +0700

    reduced code in App component

--- src/App.js
+++ src/App.js
@@ -6,19 +6,7 @@ import data from './data';
 
 export default function App() {
   const cards = data.map((item) => {
-    return (
-      <Card
-        key={item.id}
-        title={item.title}
-        location={item.location}
-        googleMapsUrl={item.googleMapsUrl}
-        startDate={item.startDate}
-        endDate={item.endDate}
-        description={item.description}
-        imageUrl={item.imageUrl}
-        directLink={item.directLink}
-      />
-    );
+    return <Card key={item.id} item={item} />;
   });
   return (
     <div className="App">
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default function Card(props) {
   return (
     <div className="Card">
-      <img src={props.imageUrl} alt={props.title} className="travel-img" />
+      <img
+        src={props.item.imageUrl}
+        alt={props.item.title}
+        className="travel-img"
+      />
       <div className="travel-text">
         <div className="location-info">
           <img
@@ -11,20 +15,20 @@ export default function Card(props) {
             alt="pin location"
             className="pin-location"
           />
-          <p className="country-location">{props.location}</p>
+          <p className="country-location">{props.item.location}</p>
           <p className="google-maps-url">
-            <a href={props.googleMapsUrl} target="_blank" rel="noreferrer">
+            <a href={props.item.googleMapsUrl} target="_blank" rel="noreferrer">
               View on Google Maps
             </a>{' '}
           </p>
         </div>
-        <h1>{props.title}</h1>
+        <h1>{props.item.title}</h1>
         <p className="travel-dates">
-          {props.startDate} - {props.endDate}
+          {props.item.startDate} - {props.item.endDate}
         </p>
         <p className="description-text">
-          {props.description}{' '}
-          <a href={props.directLink} target="_blank" rel="noreferrer">
+          {props.item.description}{' '}
+          <a href={props.item.directLink} target="_blank" rel="noreferrer">
             continue...
           </a>
         </p>
